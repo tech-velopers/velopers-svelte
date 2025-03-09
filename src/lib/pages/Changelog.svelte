6b0206c59bce2d21d903ea564280c024bbd33f7a
sangminLee <sm0514sm@gmail.com>
2025-03-09 16:46:22 +0900

    changelog 추가

--- src/lib/pages/Changelog.svelte
+++ src/lib/pages/Changelog.svelte
@@ -10,13 +10,29 @@
 
     <div class="flex gap-4">
       <div class="w-24 flex-shrink-0 pt-1">
-        <p class="text-sm font-medium text-gray-500 dark:text-gray-400">2025-03-03</p>
+        <p class="text-sm font-medium text-gray-500 dark:text-gray-400">2025-03-09</p>
       </div>
       <Alert.Root class="flex-1">
         <Alert.Title class="text-lg font-semibold">블로그 추가 🚀</Alert.Title>
         <Alert.Description>
           <p class="mt-2">
             아래 블로그들을 추가했어요! <br>
+            아이콘 하나하나 찾아내느라 힘들었어요 ㅠ <br><br>
+            ZUM, VCNC, SK C&C, PRND, NTS, IMQA, CJ온스타일, AWS, AITRICS, AB180, 휴몬랩, 화해, 그린데이터, 핏펫, 핀다, 플라네타리움, 티몬, 티맵모빌리티, 트렌비, 테이블링, 클래스101, 클라우드메이트, 크몽, 코인플러그, 코인원, 카카오헤어샵, 카카오엔터테인먼트FE, 카카오스타일, 일루넥스, 이스트소프트, 유비케어, 원티드, 오토피디아, 오일나우, 엘리시아, NC소프트 DANBI, 엔라이즈, 야놀자클라우드, 아이디어스, 씨앤텍시스템즈, 스타일쉐어, 스퀘어랩, 스캐터랩 핑퐁, 스마일게이트AI, 숨고, 센드버드, 사람인, 빅웨이브에이아이, 블로코, 브랜디, 뷰노, 부스터스, 버즈빌, 무스마, 모히톡, 메가존클라우드, 매스프레소, 매드업, 마키나락스, 뤼이드, 루닛, 로플랫, 레모네이드, 똑똑한개발자, 리멤버, 덴티움, 데이블, 더블유클럽, 꿈많은청년들, 글루시스, 그린랩스, 8퍼센트<br>  
+          </p>
+        </Alert.Description>
+      </Alert.Root>
+    </div>
+
+    <div class="flex gap-4">
+      <div class="w-24 flex-shrink-0 pt-1">
+        <p class="text-sm font-medium text-gray-500 dark:text-gray-400">2025-03-03</p>
+      </div>
+      <Alert.Root class="flex-1">
+        <Alert.Title class="text-lg font-semibold">블로그 추가 🚀</Alert.Title>
+        <Alert.Description>
+          <p class="mt-2">
+            아래 블로그들을 추가했어요! <br><br>
             비브로스, 베스핀글로벌, 컴투스, 롯데온, 모두사인, NHN, 농심DNS, 펫프렌즈, 포스타입, 삼성, SK플래닛, sqoqa, 요기요
           </p>
         </Alert.Description>
@@ -24,20 +40,24 @@
     </div>
 
     <div class="flex gap-4">
-        <div class="w-24 flex-shrink-0 pt-1">
-          <p class="text-sm font-medium text-gray-500 dark:text-gray-400">2025-02-28</p>
-        </div>
-        <Alert.Root class="flex-1">
-          <Alert.Title class="text-lg font-semibold">블로그 추가 🚀</Alert.Title>
-          <Alert.Description>
-            <p class="mt-2">
-              아래 블로그들을 추가했어요! <br>
-              한글과컴퓨터, 딜라이트룸<br>
-              제보해주신 thisis****님 감사해요!
-            </p>
-          </Alert.Description>
-        </Alert.Root>
+      <div class="w-24 flex-shrink-0 pt-1">
+        <p class="text-sm font-medium text-gray-500 dark:text-gray-400">2025-02-28</p>
       </div>
+      <Alert.Root class="flex-1">
+        <Alert.Title class="text-lg font-semibold">블로그 추가 🚀</Alert.Title>
+        <Alert.Description>
+          <p class="mt-2">
+            아래 블로그들을 추가했어요! <br><br>
+            한글과컴퓨터, 딜라이트룸<br>
+            제보해주신 thisis****님 감사해요!
+          </p>
+        </Alert.Description>
+      </Alert.Root>
+    </div>    
+    
+
+
+
 
   </div>
 </div> 
